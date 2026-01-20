eaa31b5f2326ffd6ed01f27d9b3c6260949e6f5a
LeontiiSkichko <skichko.leontii@gmail.com>
2026-01-20 10:30:50 +0000

    Initial commit

--- EveryDayBackupHomeUser.sh
+++ EveryDayBackupHomeUser.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-CONFIG_FILE="$HOME/scripts.sh/.everyday_backup_config.cfg"
-COUNT=$(echo "$BACKUPS" | wc -l)
+CONFIG_FILE="$HOME/LinuxScripts/.everyday_backup_config.cfg"
 
 if [ -f "$CONFIG_FILE" ]; then
     source "$CONFIG_FILE"
@@ -10,16 +9,6 @@ else
     exit 1
 fi
 
-if [ "$COUNT" -gt "$MAX_VALUE" ]; then
-    DELETE_COUNT=$((COUNT - MAX_VALUE))
-    OLD_BACKUPS=$(echo "$BACKUPS" | head -n "$DELETE_COUNT")
-    
-    echo "Старые бекапы собираются на пенсию..."
-    for backup in $OLD_BACKUPS; do
-        rm -rf "$BACKUP_TO/$backup"
-    done
-fi
-
 ls -t "$BACKUP_TO" | tail -n +$MAX_VALUE | xargs -I {} rm -rf "$BACKUP_TO/{}"
-tar -pczf "$BACKUP_TO/DocumentsBackup_$(date +%Y-%m-%d-%H:%M:%S).tar.gz" -C "$(dirname "$WHAT_TAKE")" "$(basename "$WHAT_TAKE")"
-echo "Backup completed: DocumentsBackup_$(date +%Y-%m-%d-%H:%M:%S).tar.gz"
+tar -pczf "$BACKUP_TO/$(date +%Y-%m-%d-%H:%M:%S).tar.gz" -C "$(dirname "$WHAT_TAKE")" "$(basename "$WHAT_TAKE")"
+echo "Backup completed: $(date +%Y-%m-%d-%H:%M:%S).tar.gz"
