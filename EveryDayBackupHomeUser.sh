dd6e9603df152781b75b80803bc245797273906e
LeontiiSkichko <skichko.leontii@gmail.com>
2026-03-28 13:23:23 +0200

    v1.00.03

--- EveryDayBackupHomeUser.sh
+++ EveryDayBackupHomeUser.sh
@@ -5,12 +5,12 @@ CONFIG_FILE="$HOME/LinuxScripts-main/.backup.cfg"
 if [ -f "$CONFIG_FILE" ]; then
     source "$CONFIG_FILE"
 else
-    echo "А где собственно сам конфиг?"
+    echo "No config - no problem?"
     exit 1
 fi
 
-((maxvalue++))
+((max_value++))
 
 ls -t "$BACKUP_TO" | grep "EDBHU.*.tar.gz" | tail -n +$max_value | xargs -I {} rm -r "$BACKUP_TO/{}"
-tar -pczf "$BACKUP_TO/EDBHU$(date +%Y-%m-%d-%H:%M:%S).tar.gz" -C "$(dirname "$WHAT_TAKE")" "$(basename "$WHAT_TAKE")"
+tar --same-owner -pczf "$BACKUP_TO/EDBHU$(date +%Y-%m-%d-%H:%M:%S).tar.gz" -C "$(dirname "$WHAT_TAKE")" "$(basename "$WHAT_TAKE")"
 echo "Backup completed: EDBHU$(date +%Y-%m-%d-%H:%M:%S).tar.gz"
