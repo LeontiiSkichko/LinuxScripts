11e21e7156316e8709de984acad46d7577b4b891
LeontiiSkichko <skichko.leontii@gmail.com>
2026-04-04 19:29:16 +0300

    v1.00.11

---
+++ EDBHUinstaller.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir EDBHU $HOME/.config/
+mv backup.cfg $HOME/.config/EDBHU/
+
+mkdir LinuxScriptEDBHU $HOME/
+mv EveryDayBackupHomeUser.sh $HOME/LinuxScriptEDBHU/
+mv .gitignore $HOME/LinuxScriptEDBHU/
