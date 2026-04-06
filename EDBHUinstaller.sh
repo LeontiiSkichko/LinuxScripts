2ff6c621ce6886a75a5603917d49023039f1069b
LeontiiSkichko <skichko.leontii@gmail.com>
2026-04-06 19:42:07 +0300

    Changed cfg on yaml, added checks for variables, added the ability to add custom names to backups

--- EDBHUinstaller.sh
+++ EDBHUinstaller.sh
@@ -4,5 +4,5 @@ path_to_main="$(pwd)"
 mkdir EDBHU $HOME/.config/
 
 cd "$path_to_main"
-mv backup.cfg $HOME/.config/EDBHU/
+mv backup.yaml $HOME/.config/EDBHU/
 mv EveryDayBackupHomeUser.sh /etc/cron.daily/
